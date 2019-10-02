beac6390e5dbc96fe43b703b29c228867c272056
Aldogro <aldogrojas5@gmail.com>
2019-10-02 16:18:53 -0300

    radio buttons

--- src/app/components/template/template.component.ts
+++ src/app/components/template/template.component.ts
@@ -1,15 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-template',
   templateUrl: './template.component.html',
   styleUrls: ['./template.component.css']
 })
-export class TemplateComponent implements OnInit {
+export class TemplateComponent {
 
-  constructor() { }
+  // tslint:disable-next-line:ban-types
+  usuario: Object = {
+    nombre: 'Aldo',
+    apellido: 'Rojas',
+    email: 'aldogrojas5@gmail.com',
+    pais: 'ARG',
+    sexo: 'Masculino'
+  };
 
-  ngOnInit() {
+  paises = [
+    {
+      codigo: 'ARG',
+      nombre: 'Argentina'
+    },
+    {
+      codigo: 'ESP',
+      nombre: 'Uruguay'
+    },
+    {
+      codigo: 'BRA',
+      nombre: 'Brasil'
+    }
+  ];
+
+  constructor() {}
+
+  guardar(forma: NgForm) {
+    console.log(forma);
   }
-
 }
