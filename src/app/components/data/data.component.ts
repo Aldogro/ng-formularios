f7e7d75cdeb246cc712494c8cec22dcabb327df3
Aldogro <49069343+Aldogro@users.noreply.github.com>
2019-10-03 22:11:12 -0300

    data forms

--- src/app/components/data/data.component.ts
+++ src/app/components/data/data.component.ts
@@ -10,12 +10,37 @@ export class DataComponent {
 
   forma: FormGroup;
 
+  usuario = {
+    nombrecompleto: {
+      nombre: 'Aldo',
+      apellido: 'Rojas'
+    },
+    email: 'aldogrojas5@gmail.com'
+  }
+
   constructor() {
+    console.log(this.usuario);
+
     this.forma = new FormGroup({
-      nombre: new FormControl('Fernando'),
-      apellido: new FormControl(),
-      email: new FormControl()
+      nombre: new FormControl('', [
+                                    Validators.required,
+                                    Validators.minLength(3)
+                                  ]/*, reglas de validacion asíncrona */),
+      apellido: new FormControl('Rojas', [
+                                          Validators.required,
+                                          Validators.minLength(3)
+                                        ]),
+      email: new FormControl('aldo.rojas@ecloudsolutions.com',
+                              [
+                                Validators.required,
+                                Validators.pattern("[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$")
+                              ])
     });
+  }
+
+  guardarCambios() {
+    console.log(this.forma.value);
+    console.log(this.forma);
   }
 
 }
