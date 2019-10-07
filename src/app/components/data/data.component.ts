939a20fea8fa87d6f479d3aa2492eacfc18542cb
Aldogro <49069343+Aldogro@users.noreply.github.com>
2019-10-06 23:34:58 -0300

    reactive forms

--- src/app/components/data/data.component.ts
+++ src/app/components/data/data.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-data',
@@ -22,27 +22,63 @@ export class DataComponent {
 
     this.forma = new FormGroup({
       nombrecompleto: new FormGroup ({
-        nombre: new FormControl( 'a' , [
+        nombre: new FormControl( '' , [
                                       Validators.required,
                                       Validators.minLength(3)
                                     ]/*, reglas de validacion asíncrona */),
-        apellido: new FormControl( 'b' , [
+        apellido: new FormControl( '' , [
                                             Validators.required,
-                                            Validators.minLength(3)
+                                            Validators.minLength(3),
+                                            this.noHerrera
                                           ])
       }),
-      email: new FormControl( 'asdf@asdf.com' ,
+      email: new FormControl( '' ,
                               [
                                 Validators.required,
                                 // tslint:disable-next-line:quotemark
                                 Validators.pattern("[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$")
-                              ])
+                              ]),
+      'pasatiempos': new FormArray([
+        new FormControl('correr', Validators.required),
+        new FormControl('comer', Validators.required),
+        new FormControl('saltar', Validators.required)
+      ]),
+      'password1': new FormControl('', Validators.required),
+      'password2': new FormControl()
     });
+
+    this.forma.get('password2').setValidators([
+      Validators.required,
+      this.noIgual.bind( this.forma )
+    ])
+  }
+
+  agregarPasatiempo() {
+    (<FormArray>this.forma.controls['pasatiempos']).push(
+      new FormControl('', Validators.required)
+    )
+  }
+
+  noHerrera( control: FormControl ): { [s:string]: boolean } {
+    if ( control.value === 'herrera' ) {
+      return {
+        noherrera: true
+      }
+    }
+    return null 
+  }
+
+  noIgual( control: FormControl ): { [s:string]: boolean } {
+    if ( control.value !== this.controls['password1'].value ) {
+      return {
+        noiguales: true
+      }
+    }
+    return null
   }
 
   guardarCambios() {
-    console.log(this.forma.get('nombrecompleto.nombre').hasError('minlength'));
-    this.forma.setValue(this.usuario);
+    console.log(this.forma.value)
   }
 
 }
